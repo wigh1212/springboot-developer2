0dbab261c06aaa9e5a32ba748a11b2ec5029431b
wigh1212 <wigh1212@naver.com>
2024-03-18 11:07:42 +0900

    google login

--- src/main/java/me/parkinje/springbootdeveloper/domain/User.java
+++ src/main/java/me/parkinje/springbootdeveloper/domain/User.java
@@ -29,11 +29,16 @@ public class User implements UserDetails { //UserDetails를 상속받아 인증 
     @Column(name="password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
+
     @Builder
-    public User(String email,String password,String auth){
+    public User(String email,String password,String auth,String nickname){
 
         this.email=email;
         this.password=password;
+        this.nickname=nickname;
     }
 
     @Override
@@ -78,5 +83,11 @@ public class User implements UserDetails { //UserDetails를 상속받아 인증 
         //계정이 사용 가능한지 확인하는 로직
         return true; //ture -> 사용 가능
     }
+
+    public User update(String nickname){
+        this.nickname=nickname;
+        return this;
+    }
+
 
 }
